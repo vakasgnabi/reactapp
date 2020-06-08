5210a43a7f38f3c7a101936be0bfaa673156d863
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-06-08 12:51:21 +0500

    new files added

--- src/Vakas.js
+++ src/Vakas.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function isReactDeveloper_fun(isReactDeveloper){
-    if(isReactDeveloper==true){
+    if(isReactDeveloper===true){
         return "React Developer";
     }
     else{
