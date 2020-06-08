67ae144bda8b258e4802c74ac5f2121db500da64
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-06-08 12:31:14 +0500

    Added new things for assignement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
+import Vakas from './Vakas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const hello=(name)=>"Hello world" + name;
+
+function App({name,age}) {
+
+  return <div>
+      Hello from App.js Updated {name} Age = {age-10}
+      <br />
+      <Hello firstName={name}></Hello>
+      <Vakas name="Vakas GN" age={31} isReactDeveloper={true} />
     </div>
-  );
 }
 
 export default App;
